5958761f239ba3d55b6b0e56532092cd54aa9d69
Tareq <tareqaldhaifani@gmail.com>
2024-11-08 00:15:16 +0400

    refactored

--- app/main.py
+++ app/main.py
@@ -5,10 +5,12 @@ from app import RedisServer
 """
 Main entry point for the Redis server.
 """
-
-if __name__ == "__main__":
+def main():
     server = RedisServer()
     try:
         asyncio.run(server.start())
     except KeyboardInterrupt:
         print("Server shutting down...")
+
+if __name__ == "__main__":
+    main()
